6165802f50bc2c12f79d0c7721742f282feb6cef
Lucyyy895 <urbanova895@gmail.com>
2025-05-31 16:27:38 +0200

    Cookies - ukladanie

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+let todos = [];
+const textInput = document.getElementById("inputText");
+
 function pridat() {
     const ul = document.getElementById("list");
     const zapis = document.getElementById("inputText").value;
-    const textInput = document.getElementById("inputText");
     if (zapis === "") {
         return;
     } 
     ul.innerHTML += `<li><input type="checkbox"><span class="text">${zapis}</span><button onclick="zmazat(this)">X</button></li>`
     textInput.value = "";
-    
+    todos.push(zapis);
+    ulozenie();
 };
 function zmazat(button) {
     const li = button.parentElement;
+    todos = todos.filter(el => el !== li);
     li.remove();
+    ulozenie();
 };
 function zmazatVsetko() {
     const zaznamy = Array.from(document.querySelectorAll('li'));
     zaznamy.forEach((li) => {
         li.remove();
     });
+    todos = [];
+    ulozenie();
+};
+
+// Ukladanie
+function ulozenie() {
+    Cookies.set('data', todos)
+};
+const saved = Cookies.get('data');
+if (saved) {
+    saved.split(",").forEach((s) => {
+        textInput.value = s;
+        pridat();
+    });
+    textInput.value = "";
 };
